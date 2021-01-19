dde00f5a264f8eea0b4d87bc26b4e8a4b4c5c54c
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2021-01-19 09:06:29 -0600

    test: stub tests for filters api

--- server/__tests__/api.spec.ts
+++ server/__tests__/api.spec.ts
@@ -6,4 +6,10 @@ describe("ShiftState Admin API", () => {
   test("GET /api/health-check returns a 200", (done) => {
     request(application).get("/api/health-check").expect(200, done);
   });
+
+  describe.skip("GET /api/filters", () => {});
+  describe.skip("GET /api/filters/:filterId", () => {});
+  describe.skip("PUT /api/filters/:filterId", () => {});
+  describe.skip("DELETE /api/filters/:filterId", () => {});
+  describe.skip("POST /api/filters", () => {});
 });
