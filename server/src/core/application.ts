819f412b01c193d020ae0c87f3f4327f2034962d
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2021-01-19 21:35:40 -0600

    refactor: minor refactor

--- server/src/core/application.ts
+++ server/src/core/application.ts
@@ -2,31 +2,27 @@
  * @module core.application
  */
 import path from "path";
-import express from "express";
-import cors from "cors";
-import helmet from "helmet";
-import compression from "compression";
-import morgan from "morgan";
+import express, { Application } from "express";
+import { IConfig } from "./configuration";
 
 const BUILD_DIR = path.resolve(__dirname, "../../../build");
 const app = express();
 
-app.use(helmet());
-app.use(cors({ origin: "*" }));
-app.use(compression());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(morgan("combined"));
-app.use(express.static(BUILD_DIR));
-
-app.get("/api/health-check", (req, res) => {
-  res.sendStatus(200);
-});
-app.get("/api/ping", (req, res) => {
-  res.json("pong");
-});
-app.use("*", (req, res) => {
-  res.sendFile(path.join(BUILD_DIR, "index.html"));
-});
+export const initialize = (configuration: IConfig): Application => {
+  configuration.LOGGER.info("Initializing express application");
+  configuration.LOGGER.info("Mounting Middleware");
+  configuration.MIDDLEWARE.forEach((middleware) => {
+    app.use(middleware);
+  });
+  configuration.LOGGER.info("Mounting Routers");
+  configuration.ROUTER_FNS.forEach((routerFns) => {
+    routerFns(app);
+  });
+  app.use(express.static(BUILD_DIR));
+  app.use("*", (req, res) => {
+    res.sendFile(path.join(BUILD_DIR, "index.html"));
+  });
+  return app;
+};
 
 export default app;
