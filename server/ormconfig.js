fe2283f02c48e80506d840d4ed177449961d2ad2
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2021-01-21 09:29:13 -0600

    build(sql): make typeorm to detect js entitites

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -2,13 +2,16 @@ const path = require("path");
 const { config } = require("dotenv");
 
 config({ path: path.resolve(__dirname, "../.env") });
-
+const entities =
+  process.env.NODE_ENV === "production"
+    ? [path.resolve(__dirname, "./**/entities/*.js")]
+    : [path.resolve(__dirname, "./**/entities/*.ts")];
 module.exports = {
   type: "postgres",
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  entities: ["src/entities/*.ts"],
-  synchronize: process.env.NODE_ENV === "production" ? false : true,
+  entities,
+  synchronize: true,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
